3ca0ab58883fac8b6738cc0439a577343e5014fb
kiniun1 <levi.marcos17@hotmail.com>
2022-05-20 15:30:00 -0300

    arquivo de teste da função de conversao de string para numeral

---
+++ test/application/test-conversao-string-numero.js
@@ -0,0 +1,31 @@
+const { expect } = require("chai");
+const { describe } = require("mocha");
+const conversaoStringNumero = require("../../applications/conversaoStringParaNumero");
+
+describe('Função de conversão de string para numeral', ()=>{
+
+    it('1º teste: Passando 1 como string, deve retornar um 1 com tipo numeral', async()=>{
+        const result = await conversaoStringNumero('1');
+        expect(result).to.be.a('number');
+        expect(result).to.equal(1);
+    });
+
+    it('2º teste: Passando -1 como string, deve retornar um -1 com tipo numeral', async()=>{
+        const result = await conversaoStringNumero('-1');
+        expect(result).to.be.a('number');
+        expect(result).to.equal(-1);
+    });
+
+    it('3º teste: Passando 2,5 como string, deve retornar um 5,5 com tipo numeral', async()=>{
+        const result = await conversaoStringNumero('2.5');
+        expect(result).to.be.a('number');
+        expect(result).to.equal(2.5);
+    });
+
+    it('4º teste: Passando -2,5 como string, deve retornar um -2,5 com tipo numeral', async()=>{
+        const result = await conversaoStringNumero('-2.5');
+        expect(result).to.be.a('number');
+        expect(result).to.equal(-2.5);
+    });
+
+});
