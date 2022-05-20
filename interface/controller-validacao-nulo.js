cb863190053bd4e7376ccd05cf04183cd12133ec
kiniun1 <levi.marcos17@hotmail.com>
2022-05-20 14:54:48 -0300

    arquivo js do controller de validação de valores nulos

---
+++ interface/controller-validacao-nulo.js
@@ -0,0 +1,21 @@
+const httpStatusResponse = require('../commons/http-response/http-status-response');
+const ValidacaoDadosVaziosOuNulos = require('./validacao-nulo');
+
+const controllerValidacaoNulos = async(dados)=>{
+    try {
+        let respostaValidacao = [];
+        for(let i = 0; i<3; i++){
+            respostaValidacao.push(await ValidacaoDadosVaziosOuNulos(dados[i]));
+        };
+        if(respostaValidacao[0] === 'ok' && respostaValidacao[1] === 'ok' && respostaValidacao[2] === 'ok'){
+            return 'ok'
+        }else{
+            return 'erro'
+        }
+    } catch (erro) {
+        const erroValidacao = await httpStatusResponse(500, (erro.message), 'validacao-operador');
+        return erroValidacao;
+    };
+};
+
+module.exports = controllerValidacaoNulos;
