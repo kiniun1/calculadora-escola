84976db96458916edea8dfcaf398464af31c3028
kiniun1 <levi.marcos17@hotmail.com>
2022-05-20 17:51:02 -0300

    arquivo teste função que valida operação de divisão por zero, e raiz a 0

--- test/application/test-controller-operacao.js
+++ test/application/test-controller-operacao.js
@@ -32,4 +32,34 @@ describe('Testando o controller que decide qual função de operação chamar', 
         const resultado = await controllerOperacao(25, '√', 2);
         expect(resultado).to.equal(5);
     });
+
+    it('7º teste: chamar função de adição com os parametros igual a 0 pra esse controller e retornar 0', async()=>{
+        const resultado = await controllerOperacao(0, '+', 0);
+        expect(resultado).to.equal(0);
+    });
+
+    it('8º teste: chamar função de subtração com os parametros igual a 0 pra esse controller e retornar 0', async()=>{
+        const resultado = await controllerOperacao(0, '-', 0);
+        expect(resultado).to.equal(0);
+    });
+
+    it('9º teste: chamar função de multiplicação com os parametros igual a 0 pra esse controller e retornar 0', async()=>{
+        const resultado = await controllerOperacao(0, '*', 0);
+        expect(resultado).to.equal(0);
+    });
+
+    it('10º teste: chamar função de divisão com o primeiro valor igual a 0 pra esse controller e retornar 0', async()=>{
+        const resultado = await controllerOperacao(0, '/', 5);
+        expect(resultado).to.equal(0);
+    });
+
+    it('11º teste: chamar função de porcentagem com os parametros igual a 0 pra esse controller e retornar 0', async()=>{
+        const resultado = await controllerOperacao(0, '%', 0);
+        expect(resultado).to.equal(0);
+    });
+
+    it('12º teste: chamar função de raiz com o primeiro valor igual a 0 pra esse controller e retornar 0', async()=>{
+        const resultado = await controllerOperacao(0, '√', 2);
+        expect(resultado).to.equal(0);
+    });
 });
---
+++ test/interface/test-validacao-operacao-zero.js
@@ -0,0 +1,27 @@
+const expect = require("chai").expect;
+const { describe } = require("mocha");
+const validacaoOperacaoZero = require("../../interface/validacao-operacao-zero");
+
+describe('Testando a validação se é divisão por 0, ou raiz de indice 0', ()=>{
+
+    it('1º teste: chamar validação passando parametros válidos deve retornar ok', async()=>{
+        const resultado = await validacaoOperacaoZero([0, '/', 2]);
+        expect(resultado).to.equal('ok');
+    });
+
+    it('2º teste: chamar validação passando parametros válidos deve retornar ok', async()=>{
+        const resultado = await validacaoOperacaoZero([0, '√', 2]);
+        expect(resultado).to.equal('ok');
+    });
+
+    it('3º teste: chamar validação passando parametros inválidos deve retornar entrada inválida', async()=>{
+        const resultado = await validacaoOperacaoZero([2, '/', 0]);
+        expect(resultado).to.equal('entrada inválida');
+    });
+
+    it('4º teste: chamar validação passando parametros inválidos deve retornar entrada inválida', async()=>{
+        const resultado = await validacaoOperacaoZero([2, '√', 0]);
+        expect(resultado).to.equal('entrada inválida');
+    });
+
+});
