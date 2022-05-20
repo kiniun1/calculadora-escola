dc9e06c1b82301da826ef875d4886f1f86e35991
kiniun1 <levi.marcos17@hotmail.com>
2022-05-20 15:40:18 -0300

    arquivo js do controller de validação de numeros

---
+++ interface/controller-validacao-numero.js
@@ -0,0 +1,40 @@
+const httpStatusResponse = require('../commons/http-response/http-status-response');
+const ValidacaoValoresSaoNumericos = require('./validacao-numero');
+const conversaoStringNumero = require('../applications/conversaoStringParaNumero');
+
+const controllerValidacaoNumeros = async(dados)=>{
+    try {
+        let respostaNumero = [];
+        let resultadoConversao = [];
+        for(let i = 0; i<2; i++){
+            respostaNumero.push(await ValidacaoValoresSaoNumericos(dados[i]));
+        }
+        
+        if(respostaNumero[0] === 'ok' && respostaNumero[1] === 'ok'){
+
+            return dados;
+
+        }else if(respostaNumero[0] === 'string' && respostaNumero[1] === 'ok'){
+
+            resultadoConversao.push(await conversaoStringNumero(dados[0]));
+            return [resultadoConversao[0], dados[1]];
+
+        }else if(respostaNumero[0] === 'ok' && respostaNumero[1] === 'string'){
+
+            resultadoConversao.push(await conversaoStringNumero(dados[1]));
+            return [dados[0], resultadoConversao[0]];
+
+        }else if(respostaNumero[0] === 'string' && respostaNumero[1] === 'string'){
+
+            resultadoConversao.push(await conversaoStringNumero(dados[0]));
+            resultadoConversao.push(await conversaoStringNumero(dados[1]));
+            return [resultadoConversao[0], resultadoConversao[1]];
+
+        }
+    } catch (erro) {
+        const erroValidacao = await httpStatusResponse(500, (erro.message), 'validacao-nulo');
+        return erroValidacao;
+    };
+};
+
+module.exports = controllerValidacaoNumeros;
