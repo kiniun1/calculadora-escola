043a9556f3cc97a2b7dcd0b99792566b6447d814
kiniun1 <levi.marcos17@hotmail.com>
2022-05-20 13:58:04 -0300

    arquivo js função de validação do operador

---
+++ interface/validacao-operador.js
@@ -0,0 +1,21 @@
+const httpStatusResponse = require('../commons/http-response/http-status-response');
+
+const ValidacaoOperador = async(dado)=>{
+    try {
+        const operadores = {
+            '+': 'ok',
+            '-': 'ok',
+            '*': 'ok',
+            '/': 'ok',
+            '%': 'ok',
+            '√': 'ok',
+        };
+        return operadores[dado] ?? "inválido";
+    } catch (erro) {
+        const erroValidacao = await httpStatusResponse(500, (erro.message), 'validacao-operador');
+        return erroValidacao;
+    }
+    
+}
+
+module.exports = ValidacaoOperador;
